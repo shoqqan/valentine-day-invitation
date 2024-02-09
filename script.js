141c7791adadc1f99d9a4e7e0ec80447970c06a8
shoqqan <tataev.shokan@gmail.com>
2024-02-10 02:35:56 +0600

    changed text in alert

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('УРААА НАПИШИ МНЕ!')
+    alert('Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно длани господней, парящей над миром? По крайней мере, истинно то, что человек не властен даже над своей волей')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
-    bottomPos=Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
-    document.getElementById('nooo').style.position="absolute"
-    document.getElementById('nooo').style.left=leftPos + "%"
-    document.getElementById('nooo').style.bottom=bottomPos + "%"
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
 })
 document.getElementById('nooo').addEventListener('click', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
-    bottomPos=Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
-    document.getElementById('nooo').style.position="absolute"
-    document.getElementById('nooo').style.left=leftPos + "%"
-    document.getElementById('nooo').style.bottom=bottomPos + "%"
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position = "absolute"
+    document.getElementById('nooo').style.left = leftPos + "%"
+    document.getElementById('nooo').style.bottom = bottomPos + "%"
 })
