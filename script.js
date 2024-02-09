505cdda1d3cf53448b1014b0f6eae6a7f74c813a
shoqqan <tataev.shokan@gmail.com>
2024-02-09 23:51:15 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let leftPos = 0
+let bottomPos = 0
+document.getElementById('yessir').addEventListener('click', () => {
+    alert('УРАА Я ЛЮБЛЮ!')
+})
+document.getElementById('nooo').addEventListener('mouseover', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos=Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position="absolute"
+    document.getElementById('nooo').style.left=leftPos + "%"
+    document.getElementById('nooo').style.bottom=bottomPos + "%"
+})
+document.getElementById('nooo').addEventListener('click', () => {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos=Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    document.getElementById('nooo').style.position="absolute"
+    document.getElementById('nooo').style.left=leftPos + "%"
+    document.getElementById('nooo').style.bottom=bottomPos + "%"
+})
